62963c5ef6af7ec027820b263bde20ae750d65ae
Jonathan Leung <chitling@gmail.com>
2026-02-23 15:02:52 -0600

    added github pages configurations

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Sandbox-app/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
